bc40c6a6d81b9c9aa5d8c0682295b831941f9ea9
Jeffrey Haen <j.haen@covadis.nl>
2018-05-14 12:45:50 +0200

    Imported API key.

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "8179e59d19ed462793759c698fcebe41";
+    var url = "https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
